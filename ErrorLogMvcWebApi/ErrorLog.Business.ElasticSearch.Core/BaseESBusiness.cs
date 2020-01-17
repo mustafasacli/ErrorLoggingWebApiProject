dbefee0b531adcad419abed0f451afd02f2a62db
mustafasacli <mustafasacli70@gmail.com>
2020-01-17 15:38:28 +0200

    Elastic Search integration added and should be tested.

---
+++ ErrorLogMvcWebApi/ErrorLog.Business.ElasticSearch.Core/BaseESBusiness.cs
@@ -0,0 +1,96 @@
+﻿namespace ErrorLog.Business.ElasticSearch.Core
+{
+    using Nest;
+    using System;
+
+    public abstract class BaseESBusiness<T> where T : class, new()
+    {
+        protected ElasticClient Client { get; set; }
+
+        public string Uri
+        { get; protected set; }
+
+        public string IndexName
+        { get; protected set; }
+
+
+
+        protected BaseESBusiness(string uri, string indexName)
+        {
+            if (string.IsNullOrWhiteSpace(uri))
+                throw new ArgumentNullException(nameof(uri));
+
+            if (string.IsNullOrWhiteSpace(indexName))
+                throw new ArgumentNullException(nameof(indexName));
+
+            this.Uri = uri;
+            this.IndexName = indexName;
+            var node = new Uri(this.Uri);
+            var settings = new ConnectionSettings(node);
+            this.Client = new ElasticClient(settings);
+        }
+
+        /// <summary>
+        /// Checks Index, if not exist create.
+        /// </summary>
+        protected virtual void CheckIndex()
+        {
+            if (!this.Client.Indices.Exists(this.IndexName).Exists)
+            {
+                this.Client.Indices.Create(this.IndexName, idx => idx.Index(this.IndexName).Map<T>(q => q.AutoMap()));
+            }
+        }
+
+        /// <summary>
+        /// Insert and return id of model.
+        /// </summary>
+        /// <param name="log"></param>
+        /// <returns></returns>
+        public string CheckExistsAndInsert(T log)
+        {
+            CheckIndex();
+            /// TODO : Elastic Search Result nesnesi için uygun geri dönüş modeli, oluşturulacak.
+            var response = this.Client.Index(log, idx => idx.Index(this.IndexName));
+            var result = response.Id;
+            return result;
+        }
+
+        /// <summary>
+        /// Gets model by with given id.
+        /// </summary>
+        /// <param name="oid"></param>
+        /// <returns></returns>
+        public T Get(string oid)
+        {
+            CheckIndex();
+            var model = new T();
+
+            if (!string.IsNullOrWhiteSpace(oid))
+            {
+                var response = this.Client.Get<T>(oid, idx => idx.Index(this.IndexName));
+                model = response.Source;
+            }
+
+            return model;
+        }
+
+        /// <summary>
+        /// Error = 0, Created = 1, Updated = 2, Deleted = 3, NotFound = 4, Noop = 5 
+        /// </summary>
+        /// <param name="oid"></param>
+        /// <returns></returns>
+        public int Delete(string oid)
+        {
+            CheckIndex();
+            var result = -10;
+
+            if (!string.IsNullOrWhiteSpace(oid))
+            {
+                var response = this.Client.Delete<T>(oid, idx => idx.Index(this.IndexName));
+                result = (int)response.Result;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ErrorLogMvcWebApi/ErrorLog.Business.ElasticSearch/ElasticSearchBusiness.cs
@@ -0,0 +1,61 @@
+﻿using ErrorLog.Business.Core.Interfaces;
+using ErrorLog.Business.ElasticSearch.Core;
+using ErrorLog.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ErrorLog.Business.ElasticSearch
+{
+    public class ElasticSearchBusiness : BaseESBusiness<ErrorLogModel>, IErrorLogBusiness
+    {
+        public ElasticSearchBusiness() :
+            base("http://localhost:9200", "change_log")
+        {
+        }
+
+        public int Delete(ErrorLogModel log)
+        {
+            var r = base.Delete(log.Id);
+            return r;
+        }
+
+        public void Dispose()
+        {
+            this.Client = null;
+        }
+
+        public ErrorLogModel GetById(string oid)
+        {
+            var result = GetById(oid);
+            return result;
+        }
+
+        public IEnumerable<ErrorLogModel> GetLogs(long? startTimestamp, long? endTimestamp)
+        {
+            var start = startTimestamp.GetValueOrDefault();
+            var end = endTimestamp.GetValueOrDefault();
+
+            /// TODO : WILL BE TESTED.
+            var rs =
+            this.Client.Search<ErrorLogModel>(s =>
+            s.Query(q =>
+            q.Range(r => r.Field(f => f.LogTimeUnixTimestamp).GreaterThanOrEquals(start))
+            && q.Range(r => r.Field(f => f.LogTimeUnixTimestamp).LessThanOrEquals(end))
+            ));
+            var rx = rs.Documents.AsEnumerable();
+            return rx;
+        }
+
+        public string Save(ErrorLogModel log)
+        {
+            var result = CheckExistsAndInsert(log);
+            return result;
+
+        }
+
+        public int Update(ErrorLogModel log)
+        {
+            return -100;
+        }
+    }
+}
